d8c41ef2de85b4ebd94c06bf9bdaf6a1ca18337a
Sharif Ally <50718667+sharibeast@users.noreply.github.com>
2022-03-14 23:50:20 +0300

    bg set

--- rebuild-codes-website/pages/index.tsx
+++ rebuild-codes-website/pages/index.tsx
@@ -1,18 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Rebuild from '../components/Rebuild.svg'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.bg}>
       <Head>
         <title>Rebuild Codes</title>
         <meta name="description" content="rebuild codes" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-      <main className={styles.main}>
-        <h1>rebuild codes website</h1>
+      <main >
+        <Image src={Rebuild} alt="logo" />
+        <div style={{ color: 'white', fontWeight: 'normal' }}>
+          <p>rebuild codes of popular websites and apps</p>
+        </div>
       </main>
     </div>
   )
